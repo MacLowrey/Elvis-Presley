a4f217de59e46e36648f36065513f3d044324195
Jon Lowrey <Jon@Jons-MacBook-Pro-3.local>
2014-04-23 20:20:24 -0500

    Initial Commit

---
+++ Elvis Presley/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Elvis Presley
+//
+//  Created by Jon Lowrey on 4/23/14.
+//  Copyright (c) 2014 Jon Lowrey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
